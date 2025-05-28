3863a7820847722fa26c0dbbdbdc8816d866c116
João Victor <joao.carli@unesp.br>
2025-05-28 09:44:11 -0300

    Sistema Barney - Competicao Meter14

--- components/ranking-tabs.tsx
+++ components/ranking-tabs.tsx
@@ -10,15 +10,15 @@ export function RankingTabs() {
   return (
     <Tabs defaultValue="dps" className="w-full">
       <TabsList className="grid w-full grid-cols-3 bg-black border border-blue-900/50">
-        <TabsTrigger value="dps" className="data-[state=active]:bg-[#00c8ff] data-[state=active]:text-black">
+        <TabsTrigger value="dps" className="data-[state=active]:bg-[#00c8ff] data-[state=active]:text-white">
           <Flame className="h-4 w-4 mr-2" />
           TOP DPS
         </TabsTrigger>
-        <TabsTrigger value="hps" className="data-[state=active]:bg-[#00c8ff] data-[state=active]:text-black">
+        <TabsTrigger value="hps" className="data-[state=active]:bg-[#00c8ff] data-[state=active]:text-white">
           <Heart className="h-4 w-4 mr-2" />
           TOP HPS
         </TabsTrigger>
-        <TabsTrigger value="participacao" className="data-[state=active]:bg-[#00c8ff] data-[state=active]:text-black">
+        <TabsTrigger value="participacao" className="data-[state=active]:bg-[#00c8ff] data-[state=active]:text-white">
           <Users className="h-4 w-4 mr-2" />
           PARTICIPAÇÃO
         </TabsTrigger>
